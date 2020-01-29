56c48697baae059daa9aa7035156ec01fe71297a
Yan <yanrus09@gmail.com>
2020-01-29 22:28:50 +0500

    Fix socket

--- web/src/common/api/index.js
+++ web/src/common/api/index.js
@@ -21,132 +21,147 @@ export function init(token, login){
 
 export function get_messages(){
   return dispatch => {
-    return new Promise((reslove, reject) =>{
+    if(socket){
+      return new Promise((reslove, reject) =>{
       
-      dispatch(actions.start_get_messages());
-
-      socket.emit('get_messages');
-
-
-      socket.on('messages', (res) =>{
-        dispatch(actions.succes_get_messages(res.data));
-        reslove(res);
-      });
-    })
+        dispatch(actions.start_get_messages());
+  
+        socket.emit('get_messages');
+  
+  
+        socket.on('messages', (res) =>{
+          dispatch(actions.succes_get_messages(res.data));
+          reslove(res);
+        });
+      })
+    }
   }
 };
 
 
 export function get_friends(){
   return dispatch => {
-    return new Promise((reslove, reject) =>{
-      
-      dispatch(actions.start_get_users());
+    if(socket){
+      return new Promise((reslove, reject) =>{
+        
+        dispatch(actions.start_get_users());
 
-      socket.emit('get_users');
+        socket.emit('get_users');
 
 
-      socket.on('users', (res) =>{
-        dispatch(actions.succes_get_users(res.data));
-        reslove(res);
-      });
-    })
+        socket.on('users', (res) =>{
+          dispatch(actions.succes_get_users(res.data));
+          reslove(res);
+        });
+      })
+    }
+    return;
   }
 };
 
 export function get_account(){
   
   return dispatch => {
-    return new Promise((reslove, reject) =>{
-      
-      dispatch(actions.start_get_account());
+    if(socket){
+      return new Promise((reslove, reject) =>{
+        
+        dispatch(actions.start_get_account());
 
-      socket.emit('get_account');
+        socket.emit('get_account');
 
 
-      socket.on('account_info', (res) =>{
-        dispatch(actions.succes_get_account(res.data));
-        reslove(res);
-      });
-    })
+        socket.on('account_info', (res) =>{
+          dispatch(actions.succes_get_account(res.data));
+          reslove(res);
+        });
+      })
+    }
   }
 }
 
 export function check_token(){
   return dispatch => {
+    if(socket){
     return new Promise((reslove, reject) =>{
       
-      dispatch(actions.start_token_check());
+        dispatch(actions.start_token_check());
 
-      socket.emit('check_token');
+        socket.emit('check_token');
 
-      socket.on('token_check_result', (res) =>{
-        if(!res.ok || !res.data){
-          dispatch(actions.failed_token_check());
-        }else{
-          dispatch(actions.success_token_check(res.data));
-        }
-        reslove(res);
+        socket.on('token_check_result', (res) =>{
+          if(!res.ok || !res.data){
+            dispatch(actions.failed_token_check());
+          }else{
+            dispatch(actions.success_token_check(res.data));
+          }
+          reslove(res);
+        });
       });
-    })
+    }
   }
 }
 
 
 export function add_friend(login){
   return dispatch => {
-    return new Promise((reslove, reject) =>{
-      
-      dispatch(actions.start_add_user());
+    if(socket){
+      return new Promise((reslove, reject) =>{
+        
+        dispatch(actions.start_add_user());
 
-      socket.emit('add_friend', {login});
+        socket.emit('add_friend', {login});
 
 
-      socket.on('ok_add_friend', (res) =>{
-        dispatch(get_friends());
-        dispatch(actions.success_add_user());
-        reslove(res);
-      });
-    })
+        socket.on('ok_add_friend', (res) =>{
+          dispatch(get_friends());
+          dispatch(actions.success_add_user());
+          reslove(res);
+        });
+      })
+    }
   }
   
 }
 
 export function delete_friend(login){
   return dispatch => {
-    return new Promise((reslove, reject) =>{
-      
-      dispatch(actions.start_delete_user());
+    if(socket){
+      return new Promise((reslove, reject) =>{
+        
+        dispatch(actions.start_delete_user());
 
-      socket.emit('delete_friend', {login});
+        socket.emit('delete_friend', {login});
 
 
-      socket.on('ok_delete_friend', (res) =>{
-        dispatch(get_friends());
-        dispatch(actions.success_delete_user());
-        reslove(res);
-      });
-    })
+        socket.on('ok_delete_friend', (res) =>{
+          dispatch(get_friends());
+          dispatch(actions.success_delete_user());
+          reslove(res);
+        });
+      })
+    }
   }
 }
 
 
 export function send_message(message, to){
   return dispatch => {
-    return new Promise((reslove, reject) =>{
-      
-      dispatch(actions.start_send_message());
+    if(socket){
+      return new Promise((reslove, reject) =>{
+        
+        dispatch(actions.start_send_message());
 
-      socket.emit('message', {
-        message,
-        to,
-      });
+        socket.emit('message', {
+          message,
+          to,
+        });
 
-      socket.on('sended_message', (res) =>{
-        dispatch(get_messages());
-        dispatch(actions.success_send_message());
-        reslove(res);
-      })  
-    })
+        socket.on('sended_message', (res) =>{
+          dispatch(get_messages());
+          dispatch(actions.success_send_message());
+          reslove(res);
+        })  
+      })
+    }
   }
 }
--- web/src/components/chat/Chat.js
+++ web/src/components/chat/Chat.js
@@ -5,18 +5,92 @@ import Main from '../main/Main';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 
+import {
+  connect,
+} from 'react-redux';
+
+import {
+  Redirect,
+} from 'react-router-dom';
+
+import {
+  get_account,
+  get_friends,
+  get_messages,
+  send_message,
+  add_friend,
+  delete_friend,
+  check_token,
+} from '../../common/api/index';
+
 class Chat extends React.Component{
-    render(){
-        return(
-            <div style={{maxWidth:"100vw"}}>
-                <Header></Header>
-                <Main>
-                    
-                </Main>
-                <Footer></Footer>
-            </div>
-        );
+  constructor(props){
+    super(props);
+
+    this.props.my_get_friend();
+    this.props.my_get_account();
+    this.props.my_get_messages();
+  }
+
+
+  render(){
+
+    let rr = "";
+
+    if(!this.props.token){
+      rr = <Redirect to={"/login"}/>
     }
+    let {users, account, messages} = this.props;
+    
+    this.props.my_check();
+
+    return(
+      <div style={{maxWidth:"100vw"}}>
+        {rr}
+        <Header></Header>
+        <Main>
+
+        </Main>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
 
-export default Chat;
+const mapStateToProps = state => {
+  return {
+    token: state.token,
+    messages: state.messages,
+    users: state.users,
+    account: state.account,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    my_get_friend: () => {
+      dispatch(get_friends());
+    },
+    my_get_account: () => {
+      dispatch(get_account());
+    },
+    my_get_messages: () => {
+      dispatch(get_messages());
+    },
+    my_add_friend: (log) => {
+      dispatch(add_friend(log));
+    },
+    my_delete_friend: (log) => {
+      dispatch(delete_friend(log))
+    },
+    my_send_message: (msg, to) => {
+      dispatch(send_message(msg, to));
+    },
+    my_check: () => {
+      dispatch(check_token());
+    }
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chat);
