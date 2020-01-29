0d5c85887cb51cebf53f1dce855cecdd60296feb
Yan <yanrus09@gmail.com>
2020-01-30 02:10:16 +0500

    Remake header

--- web/src/components/header/Header.js
+++ web/src/components/header/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import {
+    connect
+} from 'react-redux';
 import "./header.scss";
 
 class Header extends React.Component{
-    render(){
-        return (
-            <div id="header">
-                <p>Чат-приложение</p>
-            </div>
-        )
+  render(){
+    console.log('account', this.props.account);
+    let {account} = this.props;
+    return (
+      <div id="header">
+        <div>{account.login ? `Здравствуй, ${account.login}` : "Чат-приложение"}</div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state =>{
+    return {
+        account: state.account,
     }
 }
 
-export default Header;
+export default connect(mapStateToProps)(Header);
