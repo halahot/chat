03b35f239616290445d188434702484fbab58a30
Yan <yanrus09@gmail.com>
2020-02-28 08:58:37 +0500

    Change ip

--- web/src/common/api/index.js
+++ web/src/common/api/index.js
@@ -4,7 +4,7 @@ import * as actions from '../redux/actions';
 const io = require("socket.io-client");
 
 
-const ip = "84.201.173.168"
+const ip = "84.201.130.48"
 const Sport = 9099;
 
 let socket;
--- web/src/common/api/registrator_api.js
+++ web/src/common/api/registrator_api.js
@@ -4,7 +4,7 @@ import * as actions from '../redux/actions';
 const axios = require("axios");
 const bcrypt = require("bcryptjs");
 
-const ip = "84.201.173.168"
+const ip = "84.201.130.48"
 
 const Rport = 9025;
 const adress = `http://${ip}:${Rport}`;
